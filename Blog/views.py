f111388ee9bc69ba26be0f399ff0587bfe75e7a8
Sakii Mia <Sakilmia@gmail.com>
2021-01-07 05:17:22 -0600

    add pagination in index

--- Blog/views.py
+++ Blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.core.paginator import Paginator,EmptyPage
 
 # Create your views here.
 from .models import BlogPost,Category
@@ -11,16 +12,32 @@ def index(request):
     cats = Category.objects.all()
 
     if 'category' in request.GET:
-        posts = posts.filter(category = int(request.GET.get('category')))
+        page = posts.filter(category = int(request.GET.get('category')))
+        pageNumber = 1
+    elif 'postid' in request.GET:
+        page = posts.filter(id = int(request.GET.get('postid')))
 
-    if 'postid' in request.GET:
-        posts = posts.filter(id = int(request.GET.get('postid')))
-
+    elif 'post' not in request.GET:
+        posts = Paginator(posts, 5)
+        pageNumber = request.GET.get('page', 1)
+        try:
+            page = posts.page(pageNumber)
+        except EmptyPage:
+            page = posts.page(pageNumber)
     context = {
-        'posts' : posts.order_by('-created_at'),
-        'cats' : cats
+        'posts': page,
+        'cats': cats,
+        'pn': pageNumber
     }
-    return render(request,'index.html',context)
+    return render(request, 'index.html', context)
+
+
+
+def category(request):
+
+    return render(request,'category.html')
+
+
 
 def view_post(request):
     post = BlogPost.objects.get(pk=int(request.GET.get('postid')))
